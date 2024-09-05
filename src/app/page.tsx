5884bddd158be763ab2b2d094bf755feae96c9ea
Manish1407 <manish2012dehraj@gmail.com>
2024-09-05 15:54:47 +0100

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import FeaturedSection from "@/components/FeaturedSection";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import TestimonialCards from "@/components/TestimonialCards";
+import UpcomingWebinar from "@/components/UpcomingWebinar";
 import WhyChoose from "@/components/WhyChoose";
-
 
 export default function Home() {
   return (
-   <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-
-    <HeroSection/>
-<FeaturedSection/>
-<WhyChoose/>
-<TestimonialCards/>
-   </main>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
+      <HeroSection />
+      <FeaturedSection />
+      <WhyChoose />
+      <TestimonialCards />
+      <UpcomingWebinar />
+      <Instructors/>
+      <Footer/>
+    </main>
   );
 }
